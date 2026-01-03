26ebae80f916e671ca153f2aadf82ba9e25a5f31
root <root@ToZo.localdomain>
2026-01-03 23:07:19 +0530

    Revised commit

--- Day-09/First_Boto3.py
+++ Day-09/First_Boto3.py
@@ -1,13 +1,36 @@
-import boto3 
+# Creation of Bucket with real life AWS credentials
 
-client = boto3.client('s3', region_name='us-east-1')  
+# first we have to import boto3 library
+import boto3
 
-response = client.create_bucket(
-    Bucket='vishal-demo-123', 
+print("=== Creating My First bucket  ===")
+
+# We have to connect first to S3 service WITH REGION
+s3 = boto3.client('s3', region_name='ap-south-1')
+
+# ask for bucket name
+bucket_name = input("Enter your bucket name: (must be unique all times): ").strip().lower()
+
+# create the bucket WITH LocationConstraint
+s3.create_bucket(
+    Bucket=bucket_name,
+    CreateBucketConfiguration={
+        'LocationConstraint': 'ap-south-1'  
+
+    }
 )
 
-print("Bucket created successfully!")
-print("Bucket owner: ")
-print(f" ID: {response['Owner']['ID'
-]}")
-print(f"  Display Name: {response['Owner'].get('DisplayName', 'N/A')}")
+print(f"✅ Bucket -> {bucket_name} created successfully in ap-south-1!")
+
+#  this is how we upoload a file to the bucket
+file_name = "my_test_file.txt"
+with open(file_name, 'w') as f:
+    f.write(f"This file was uploaded to bucket: {bucket_name}\n")
+    f.write("Created using Boto3 Python script.\n")
+
+print(f"\n📄 Created test file: {file_name}")
+
+# Upload the file
+s3.upload_file(file_name, bucket_name, file_name)
+print(f"✅ Uploaded {file_name} to bucket {bucket_name}")
+print(f"\n Access your file at:")
